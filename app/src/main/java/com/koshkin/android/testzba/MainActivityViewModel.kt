f7994afc6b5a048ce8c09ed7a3d097cbf1d38790
otarik <otarr20@gmail.com>
2023-01-16 00:12:27 +0300

    changed MainActivityViewModel.kt

--- app/src/main/java/com/koshkin/android/testzba/MainActivityViewModel.kt
+++ app/src/main/java/com/koshkin/android/testzba/MainActivityViewModel.kt
@@ -2,8 +2,18 @@ package com.koshkin.android.testzba
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.koshkin.android.testzba.data.api.Bank
+import com.koshkin.android.testzba.data.api.Country
+import com.koshkin.android.testzba.data.api.Number
 
 class MainActivityViewModel : ViewModel() {
-    val title = MutableLiveData<String>()
-    val description = MutableLiveData<String>()
+    //  val title = MutableLiveData<String>()
+    //  val description = MutableLiveData<String>()
+    val number = MutableLiveData<Number>()
+    val scheme = MutableLiveData<String>()
+    val type = MutableLiveData<String>()
+    val brand = MutableLiveData<String>()
+    val prepaid = MutableLiveData<Boolean>()
+    val country = MutableLiveData<Country>()
+    val bank = MutableLiveData<Bank>()
 }
